f916ab6e50ec6f381c1257c5e99d74f654db737a
Shawn Hurley <smhurley00@gmail.com>
2018-08-01 13:21:21 -0400

    Moving to new org.

--- tmp/codegen/update-generated.sh
+++ tmp/codegen/update-generated.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DOCKER_REPO_ROOT="/go/src/github.com/automationbroker/ansible-operator"
+DOCKER_REPO_ROOT="/go/src/github.com/water-hole/ansible-operator"
 IMAGE=${IMAGE:-"gcr.io/coreos-k8s-scale-testing/codegen:1.9.3"}
 
 docker run --rm \
@@ -13,8 +13,8 @@ docker run --rm \
   "$IMAGE" \
   "/go/src/k8s.io/code-generator/generate-groups.sh"  \
   "deepcopy" \
-  "github.com/automationbroker/ansible-operator/pkg/generated" \
-  "github.com/automationbroker/ansible-operator/pkg/apis" \
+  "github.com/water-hole/ansible-operator/pkg/generated" \
+  "github.com/water-hole/ansible-operator/pkg/apis" \
   "app:v1alpha1" \
   --go-header-file "./tmp/codegen/boilerplate.go.txt" \
   $@
