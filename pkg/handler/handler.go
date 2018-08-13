8c1c3450eccb8813483499660d011929776e2fa8
Shawn Hurley <smhurley00@gmail.com>
2018-08-13 10:19:07 -0400

    adding better godocs

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -14,8 +14,11 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// Interface used to handle a runner event. This will be called if a mapping is found
-// for the GVK of the event.
+// Interface used to handle a runner event. This will be called if a
+// mapping is found for the GVK of the event. This will be used as an override
+// for the default handle implementation. To set this for the handler, set the
+// options Handle with your implementation of this interface, and it will be
+// used.
 type Interface interface {
 	Handle(context.Context, sdk.Event, runner.Runner) error
 }
@@ -77,13 +80,19 @@ func defaultHandle(ctx context.Context, event sdk.Event, run runner.Runner) erro
 	return nil
 }
 
-// Options will be used to tell the new ansible handler how to behave.
+// Options will be used to tell the new ansible handler how to behave. You have
+// the ability to set the Interface that will be used to handle the sdk.Event.
+// The GVKToRunner map must be passed in and must have at least a single
+// mapping.
 type Options struct {
 	Handle      Interface
 	GVKToRunner map[schema.GroupVersionKind]runner.Runner
 }
 
-// New will create a ansible handler to be used by the sdk.
+// New will create a ansible handler to be used by the sdk. New will create a
+// sdk.Handler that will manage the GVKToRunner map to send the correct runner
+// to the handle Method of the Interface interface. A default handle will be
+// used if one is not set in the options.
 func New(options Options) (sdk.Handler, error) {
 	if len(options.GVKToRunner) == 0 {
 		return nil, fmt.Errorf("options must contain a gvk runner mapping")
