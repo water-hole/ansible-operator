8e06c2fb7ca0b36cb601c42d86a37edebd404bf5
Shawn Hurley <smhurley00@gmail.com>
2018-08-17 09:37:51 -0400

    Refactor handler package to hide implementation but hooks (#26)
    * This will handle default cases
    * Better handle the error situation when we would never do anything
    * Better layout for changing the default behavior.

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
@@ -13,21 +14,24 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-func New(m map[schema.GroupVersionKind]runner.Runner) sdk.Handler {
-	return &Handler{crdToPlaybook: m}
+// EventHandler used to handle a runner event. This will be called if a
+// mapping is found for the GVK of the event. This will be used as an override
+// for the default handle implementation. To set this for the handler, set the
+// options Handle with your implementation of this interface, and it will be
+// used.
+type EventHandler interface {
+	Handle(context.Context, sdk.Event, runner.Runner) error
 }
 
-type Handler struct {
-	crdToPlaybook map[schema.GroupVersionKind]runner.Runner
-	// Fill me
+// EventHandlerFunc is a adapter to use functions as handlers.
+type EventHandlerFunc func(context.Context, sdk.Event, runner.Runner) error
+
+// Handle calls f(ctx, event, run)
+func (f EventHandlerFunc) Handle(ctx context.Context, event sdk.Event, run runner.Runner) error {
+	return f(ctx, event, run)
 }
 
-func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
-	p, ok := h.crdToPlaybook[event.Object.GetObjectKind().GroupVersionKind()]
-	if !ok {
-		logrus.Warnf("unable to find playbook mapping for gvk: %v", event.Object.GetObjectKind().GroupVersionKind())
-		return nil
-	}
+func defaultHandle(ctx context.Context, event sdk.Event, run runner.Runner) error {
 	u, ok := event.Object.(*unstructured.Unstructured)
 	if !ok {
 		logrus.Warnf("object was not unstructured - %#v", event.Object)
@@ -54,7 +58,7 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 		logrus.Warnf("spec is not a map[string]interface{} - %#v", s)
 		return nil
 	}
-	statusEvent, err := p.Run(spec, u.GetName(), u.GetNamespace(), kc.Name())
+	statusEvent, err := run.Run(spec, u.GetName(), u.GetNamespace(), kc.Name())
 	if err != nil {
 		return err
 	}
@@ -74,4 +78,43 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 		return nil
 	}
 	return nil
+}
+
+// Options will be used to tell the new ansible handler how to behave. You have
+// the ability to set the Interface that will be used to handle the sdk.Event.
+// The GVKToRunner map must be passed in and must have at least a single
+// mapping.
+type Options struct {
+	Handle      EventHandler
+	GVKToRunner map[schema.GroupVersionKind]runner.Runner
+}
+
+// New will create a ansible handler to be used by the sdk. New will create a
+// sdk.Handler that will manage the GVKToRunner map to send the correct runner
+// to the handle Method of the Interface interface. A default handle will be
+// used if one is not set in the options.
+func New(options Options) (sdk.Handler, error) {
+	if len(options.GVKToRunner) == 0 {
+		return nil, fmt.Errorf("options must contain a gvk runner mapping")
+	}
+	var handle EventHandler = EventHandlerFunc(defaultHandle)
+	if options.Handle != nil {
+		handle = options.Handle
+	}
+	return &handler{crdToPlaybook: options.GVKToRunner, handle: handle}, nil
+}
+
+type handler struct {
+	crdToPlaybook map[schema.GroupVersionKind]runner.Runner
+	handle        EventHandler
+}
+
+// Handle conform to the sdk.Handle interface.
+func (h *handler) Handle(ctx context.Context, event sdk.Event) error {
+	p, ok := h.crdToPlaybook[event.Object.GetObjectKind().GroupVersionKind()]
+	if !ok {
+		logrus.Warnf("unable to find playbook mapping for gvk: %v", event.Object.GetObjectKind().GroupVersionKind())
+		return nil
+	}
+	return h.handle.Handle(ctx, event, p)
 }
