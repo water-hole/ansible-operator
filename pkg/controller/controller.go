04af36b6b51f324892516f9f280e7a4902446ade
Shawn Hurley <smhurley00@gmail.com>
2018-09-05 16:04:08 -0400

    adding options for the new controller

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -17,30 +17,46 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-func NewController(mrg manager.Manager, gvk schema.GroupVersionKind, namespace string, runner runner.Runner) {
-	logrus.Infof("Watching %s/%v, %s, %s", gvk.Group, gvk.Version, gvk.Kind, namespace)
-	h := &ReconcileAnsibleOperator{
-		Client: mrg.GetClient(),
-		GVK:    gvk,
-		Runner: runner,
-		EventHandlers: []events.EventHandler{
-			events.NewLoggingEventHandler(events.Tasks),
-		},
+// Options - options for your controller
+type Options struct {
+	EventHandlers []events.EventHandler
+	LoggingLevel  events.LogLevel
+	Runner        runner.Runner
+	Namespace     string
+	GVK           schema.GroupVersionKind
+}
+
+// NewController - Creates a new ansible operator controller and adds it to the manager
+func NewController(mrg manager.Manager, options Options) {
+	logrus.Infof("Watching %s/%v, %s, %s", options.GVK.Group, options.GVK.Version, options.GVK.Kind, options.Namespace)
+	if options.EventHandlers == nil {
+		options.EventHandlers = []events.EventHandler{}
 	}
-	mrg.GetScheme().AddKnownTypeWithName(gvk, &unstructured.Unstructured{})
+	eventHandlers := append(options.EventHandlers, events.NewLoggingEventHandler(options.LoggingLevel))
+
+	h := &ReconcileAnsibleOperator{
+		Client:        mrg.GetClient(),
+		GVK:           options.GVK,
+		Runner:        options.Runner,
+		EventHandlers: eventHandlers,
+	}
+
+	// Register the GVK with the schema
+	mrg.GetScheme().AddKnownTypeWithName(options.GVK, &unstructured.Unstructured{})
 	metav1.AddToGroupVersion(mrg.GetScheme(), schema.GroupVersion{
-		Group:   gvk.Group,
-		Version: gvk.Version,
+		Group:   options.GVK.Group,
+		Version: options.GVK.Version,
 	})
-	//Create new controllers for each gvk.
-	c, err := controller.New(fmt.Sprintf("%v-controller", strings.ToLower(gvk.Kind)), mrg, controller.Options{
+
+	//Create new controller runtime controller and set the controller to watch GVK.
+	c, err := controller.New(fmt.Sprintf("%v-controller", strings.ToLower(options.GVK.Kind)), mrg, controller.Options{
 		Reconciler: h,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	u := &unstructured.Unstructured{}
-	u.SetGroupVersionKind(gvk)
+	u.SetGroupVersionKind(options.GVK)
 	if err := c.Watch(&source.Kind{Type: u}, &crthandler.EnqueueRequestForObject{}); err != nil {
 		log.Fatal(err)
 	}
