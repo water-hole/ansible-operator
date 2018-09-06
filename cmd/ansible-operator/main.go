a4d0225f399c683be50e033f2c6e30a51abca58d
Shawn Hurley <smhurley00@gmail.com>
2018-09-06 15:52:10 -0400

    Ansible Operator using controller runtime (#44)
    * rewrite AO with controller runtime
    
    * keep working on controller runtime refactor.
    
    * fixing Gopkg
    
    * adding options for the new controller
    
    * addressing PR comments
    
    * Conforming with controller runtime expectations for controller package
    
    * Change New to Add
    
    * rebase controller runtime

--- cmd/ansible-operator/main.go
+++ cmd/ansible-operator/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"flag"
+	"log"
 	"math/rand"
 	"runtime"
 	"time"
 
-	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
-	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
-	handler "github.com/water-hole/ansible-operator/pkg/handler"
+	"github.com/water-hole/ansible-operator/pkg/controller"
 	proxy "github.com/water-hole/ansible-operator/pkg/proxy"
 	"github.com/water-hole/ansible-operator/pkg/runner"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	k8sruntime "k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
 
 	"github.com/sirupsen/logrus"
 )
@@ -28,20 +26,29 @@ func printVersion() {
 }
 
 func main() {
+	flag.Parse()
+	logf.SetLogger(logf.ZapLogger(false))
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	printVersion()
 	done := make(chan error)
 
 	// start the proxy
 	proxy.RunProxy(done, proxy.Options{
-		Address: "localhost",
-		Port:    8888,
+		Address:    "localhost",
+		Port:       8888,
+		KubeConfig: mgr.GetConfig(),
 	})
 
 	// start the operator
-	go runSDK(done)
+	go runSDK(done, mgr)
 
 	// wait for either to finish
-	err := <-done
+	err = <-done
 	if err == nil {
 		logrus.Info("Exiting")
 	} else {
@@ -49,33 +56,8 @@ func main() {
 	}
 }
 
-func registerGVK(gvk schema.GroupVersionKind) {
-	schemeBuilder := k8sruntime.NewSchemeBuilder(func(s *k8sruntime.Scheme) error {
-		s.AddKnownTypeWithName(gvk, &unstructured.Unstructured{})
-		return nil
-	})
-	k8sutil.AddToSDKScheme(schemeBuilder.AddToScheme)
-}
-
-// Used to set the decoder to just deserialize instead of decode.
-// This is only need as a work around for now and when the SDK is based on
-// controller-runtime we expect this to be fixed.
-// This issue https://github.com/operator-framework/operator-sdk/issues/382
-// is tracking this process.
-func decoder(gv schema.GroupVersion, codecs serializer.CodecFactory) k8sruntime.Decoder {
-	return codecs.UniversalDeserializer()
-}
-
-func runSDK(done chan error) {
-	// setting the utility decoder function.
-	k8sutil.SetDecoderFunc(decoder)
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		logrus.Error("Failed to get watch namespace")
-		done <- err
-		return
-	}
-	resyncPeriod := 60
+func runSDK(done chan error, mgr manager.Manager) {
+	namespace := "default"
 	watches, err := runner.NewFromWatches("/opt/ansible/watches.yaml")
 	if err != nil {
 		logrus.Error("Failed to get watches")
@@ -84,21 +66,13 @@ func runSDK(done chan error) {
 	}
 	rand.Seed(time.Now().Unix())
 
-	for gvk := range watches {
-		logrus.Infof("Watching %s/%v, %s, %s, %d", gvk.Group, gvk.Version, gvk.Kind, namespace, resyncPeriod)
-		registerGVK(gvk)
-		sdk.Watch(fmt.Sprintf("%v/%v", gvk.Group, gvk.Version), gvk.Kind, namespace, resyncPeriod)
-
+	for gvk, runner := range watches {
+		controller.Add(mgr, controller.Options{
+			GVK:       gvk,
+			Namespace: namespace,
+			Runner:    runner,
+		})
 	}
-	h, err := handler.New(handler.Options{
-		GVKToRunner: watches,
-	})
-	if err != nil {
-		logrus.Errorf("unable to create ansible handler - %v", err)
-		done <- err
-		return
-	}
-	sdk.Handle(h)
-	sdk.Run(context.TODO())
+	log.Fatal(mgr.Start(signals.SetupSignalHandler()))
 	done <- nil
 }
